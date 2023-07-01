8ddc8963c73dbfe0e7b4b60a4963b3e792040510
Alex Popov <hello@alexpopov.ca>
2023-07-01 09:19:32 +0200

    mac_support: KOReader paths

--- config/bash/mac_support.sh
+++ config/bash/mac_support.sh
@@ -1,3 +1,5 @@
+# Don't forget to export PATH at the end
+
 function jk_mac_fix_brew {
     echo "running sudo chown..."
     sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/sbin
@@ -7,3 +9,26 @@ function jk_mac_fix_brew {
 # Add bash keybindings for fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
+PATH="/opt/homebrew/bin:$PATH"
+
+# May want to gate this in the future:
+
+# START: tools to build KOReader
+
+PATH="/opt/homebrew/opt/bison/bin:$PATH"
+# For compilers to find bison you may need to set:
+#  export LDFLAGS="-L/opt/homebrew/opt/bison/lib"
+
+PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
+PATH="/opt/homebrew/opt/libtool/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+
+PATH="/opt/homebrew/opt/binutils/bin:$PATH"
+# For compilers to find binutils you may need to set:
+#   export LDFLAGS="-L/opt/homebrew/opt/binutils/lib"
+#   export CPPFLAGS="-I/opt/homebrew/opt/binutils/include"
+
+# END: tools to build KOReader
+
+export PATH
