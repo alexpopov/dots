fcfde3601a6ceeaf9d05e944d4a7ff81115ca471
Alex Popov (VROS) <alexpopov@meta.com>
2023-07-05 14:32:25 +0200

    sanity_check: add lazygit

--- config/bash/sanity_check.sh
+++ config/bash/sanity_check.sh
@@ -62,6 +62,14 @@ then
     echo -e "  and install from package manager"
 fi
 
+if ! command -v lazygit &> /dev/null
+then
+    echo -e "${color_yellow}WARNING:$color_reset lazygit not installed"
+    echo -e "  nice little tui for git"
+    echo -e "  ADVICE: go to$color_purple" 'https://github.com/jesseduffield/lazygit' $color_reset
+    echo -e "  and install from package manager"
+fi
+
 if [[ -n $NVIM_PYTHON ]]; then
   if ! [[ -f $NVIM_PYTHON ]]; then
     echo -e "${color_yellow}WARNING:$color_reset NVIM_PYTHON set but invalid"
