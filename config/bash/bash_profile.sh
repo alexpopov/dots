69c7f15bd20795fd292d0a9ca274b7b62dfeb622
Alex Popov <hello@alexpopov.ca>
2023-12-03 09:38:08 +0100

    fix homebrew path on home mac

--- config/bash/bash_profile.sh
+++ config/bash/bash_profile.sh
@@ -15,8 +15,10 @@ alias lg=lazygit
 PS1="$PROMPT_PREFIX\[\033[1;38m\]\]\[\$\] \[\033[0;38m\]"
 export PS1="\n$PS1"  # Add a new line so it's easier to find where long command start/end
 
-PATH=$HOME/.local/bin:$PATH
-PATH=$PATH:$HOME/.local/bin/scripts
+PATH="$HOME/.local/bin:$PATH"
+PATH="$PATH:$HOME/.local/bin/scripts"
+PATH="$PATH:/opt/homebrew/opt/bin/"
+PATH="$PATH:/opt/homebrew/bin/"
 export PATH
 export NODE_PATH="/usr/local/lib/node_modules"
 # Set this to what editor you want to use if
