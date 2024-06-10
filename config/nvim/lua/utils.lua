0178e5ad2ebf426daf440ea9d221f98c7b98829e
Alex Popov <alexpopov@fb.com>
2024-06-10 14:22:29 +0200

    vim: create scratch buffer

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -9,4 +9,12 @@ function M.load_latest_session()
   require("persistence").load()
 end
 
+function M.create_scratch_buffer()
+  local buf = vim.api.nvim_create_buf(true, true)
+  vim.api.nvim_buf_set_name(buf, "[scratch]")
+  vim.bo[buf.bufnr()].buftype = "nofile"
+  vim.bo[buf.bufnr()].bufhidden = "hide"
+  vim.bo[buf.bufnr()].noswapfile = "true"
+end
+
 return M
