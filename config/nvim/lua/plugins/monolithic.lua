25312e515d2bb2d60989d5152a151c78ce3c8caf
Alex Popov <alexpopov@fb.com>
2024-11-08 10:07:33 +0100

    vim: trouble trouble

--- config/nvim/lua/plugins/monolithic.lua
+++ config/nvim/lua/plugins/monolithic.lua
@@ -132,8 +132,41 @@ local specs = {
   },
 
   {
+    -- Doesn't work: I get errors. maybe try again later
     "folke/trouble.nvim",
     cmd = "Trouble",
+    keys = {
+      {
+        "<leader>txx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>txX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tcs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tcl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>txL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>txQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
 
   {"tpope/vim-commentary"},
