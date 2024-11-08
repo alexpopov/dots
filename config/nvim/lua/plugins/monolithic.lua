4317a588c18520ee85c3428daee8039e0174c6c3
Alex Popov <alexpopov@fb.com>
2024-11-08 09:52:31 +0100

    vim: simplify nvim-autopairs import

--- config/nvim/lua/plugins/monolithic.lua
+++ config/nvim/lua/plugins/monolithic.lua
@@ -122,18 +122,9 @@ local specs = {
   },
 
   {
-    "windwp/nvim-autopairs",
-    dependencies = "nvim-cmp",
-    config = function()
-      require("nvim-autopairs").setup({})
-
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on(
-      "confirm_done",
-      cmp_autopairs.on_confirm_done({ map_char = { tex = "" } })
-      )
-    end,
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
   },
 
   {
