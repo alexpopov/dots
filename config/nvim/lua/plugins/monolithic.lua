c9492e0fe73bb45a21596bfa0246d3e15e915897
Alex Popov <alexpopov@fb.com>
2024-11-08 10:16:37 +0100

    vim: formatting

--- config/nvim/lua/plugins/monolithic.lua
+++ config/nvim/lua/plugins/monolithic.lua
@@ -292,10 +292,7 @@ local specs = {
   {
     "folke/persistence.nvim",
     event = "BufReadPre",     -- this will only start session saving when an actual file was opened
-    module = "persistence",
-    config = function()
-      require("persistence").setup()
-    end,
+    opts = { },
   },
 
   { "powerman/vim-plugin-AnsiEsc" },
