9e10ded296889d541b280e0073a70b2afe38d2e2
Alex Popov <alexpopov@meta.com>
2025-01-09 05:31:37 -0800

    nvim: treesitter: make lazy

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
+    lazy = true,
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
