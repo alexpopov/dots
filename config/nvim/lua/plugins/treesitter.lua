bd6219d3d650446dbfe439be73f419ab61b5d681
Alex Popov <alexpopov@fb.com>
2025-01-16 15:18:06 +0100

    nvim: stop updating treesitter every time

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,22 @@
+-- TODO: Make this configurable
+local update_treesitter = false
+local ensure_installed
+if (update_treesitter == true)  then
+  ensure_installed = {
+          "bash", "c", "cpp", "lua", "rust", "cmake", "comment", "go", "java", "javascript", "json",
+          "make", "python", "regex", "vim", "yaml", "kotlin", "markdown", "markdown_inline"
+        }
+else
+  ensure_installed = {}
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = true,
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "c", "cpp", "lua", "rust", "cmake", "comment", "go", "java", "javascript", "json",
-          "make", "python", "regex", "vim", "yaml", "kotlin", "markdown", "markdown_inline"
-        },
+        ensure_installed = ensure_installed,
         auto_install = false,
         highlight = {
           enable = true,
