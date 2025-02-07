9bc121e6e26d6335c3d4ba54ee1ec18c81537c8f
Alex Popov (VROS) <alexpopov@meta.com>
2025-02-07 14:35:49 +0100

    add gitconfig aliases and bootstrap it

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,9 +12,11 @@ ln -sf "$DOTS_CONFIG_DIR/input" "$CONFIG_DIR"
 ln -sf "$DOTS_CONFIG_DIR/lazygit" "$CONFIG_DIR"
 ln -sf "$DOTS_CONFIG_DIR/karabiner" "$CONFIG_DIR"
 ln -sf "$DOTS_CONFIG_DIR/nvim" "$CONFIG_DIR"
+ln -sf "$DOTS_CONFIG_DIR/git" "$CONFIG_DIR"
 ln -sf "$DOTS_CONFIG_DIR/tmux" "$CONFIG_DIR"  # tmux refuses to use XDG, this is for us to have tmux.conf
 ln -sf "$DOTS_CONFIG_DIR/input" "$CONFIG_DIR"   # forwards config
 ln -sfn "$DOTS_CONFIG_DIR/tmux" "$HOME/.tmux"  # tmux folder for tmux's sake
+
 ln -sf "$DOTS_DIR/tmux.conf" "$HOME/.tmux.conf" # this one just forwards config
 ln -sf "$DOTS_DIR/inputrc" "$HOME/.inputrc"   # forwards config
 
