c8b18285a0b55044ae92d9fb73d2b7622066b109
Alex Popov (VROS) <alexpopov@meta.com>
2025-02-08 17:44:03 +0100

    tmux: macbook startup script complete
    the numbering of panes is insane

--- bin/scripts/tmux/tmux_macbook_main.sh
+++ bin/scripts/tmux/tmux_macbook_main.sh
@@ -3,28 +3,46 @@
 session="Main"
 window_yabai="yabai"
 
-t_window_yabai="=$session:=$window_yabai"
+t_window_yabai="$session:1"
 
-tmux new-session -d -s "$session" -n "$window_yabai"
+if ! tmux has-session -t "$session" 2>/dev/null; then
+  # create the session
+  tmux new-session -d -s "$session"
 
-tmux rename-window -t "$t_window_yabai" "yabai"
+  tmux rename-window -t "$session:1" "$window_yabai"
+  tmux split-window -h  # implicitly the first window
+  tmux split-window -v -t "$session:1.0"
+  tmux split-window -v -t "$session:1.2"
+fi
 
-# [ A | B ]
-# [ C | D ]
+# tmux attach -t "$session"
+
+# Now the panes are numbered:
+# [ 0 | 2 ]
+# [ 1 | 3 ]
 #
-# A: skhd
-# C: yabai
-# B: dots vim
-# D: dots dir
+# Set up as follows:
+# 0: skhd
+# 1: yabai
+# 2: dots vim
+# 3: dots dir
 
-# Pane B
-tmux split-window -t "$t_window_yabai" -h
-tmux send-keys -t "$t_window_yabai" 'cd ~/dots/; nvim' Enter
-tmux split-window -t "$t_window_yabai" -v
+tmux send-keys -t "$t_window_yabai.0" '$(which skhd) --config ~/dots/config/skhd/skhdrc' C-m
+tmux send-keys -t "$t_window_yabai.1" 'yabai' C-m
+tmux send-keys -t "$t_window_yabai.2" 'cd dots; vim config/nvim/lua/lua_init.lua' C-m
+tmux send-keys -t "$t_window_yabai.3" 'cd dots' C-m
 
-# Pane A
-tmux select-pane -t 0
-tmux send-keys -t "$t_window_yabai" '$(which skhd) --config ~/dots/config/skhd/skhdrc' Enter
-tmux split-window -v
-# Pane C
-tmux send-keys -t "$t_window_yabai" 'yabai' Enter
+
+
+# # In Pane 1
+# tmux split-window -t "$t_window_yabai" -h # split into left and right
+# # In Pane 2
+# tmux send-keys -t "$t_window_yabai" 'cd ~/dots/; nvim' Enter
+
+# # Pane 1
+# tmux select-pane -t 0
+# tmux split-window -v
+# # Pane C
+# tmux send-keys -t "$t_window_yabai" 'yabai' Enter
+# tmux select-pane -t
+# tmux split-window -t "$t_window_yabai" -v
