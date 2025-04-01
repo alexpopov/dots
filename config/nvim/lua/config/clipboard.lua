844f42e10107dda2053ce0b1cc8e0e02df985f14
Alex Popov <alexpopov@fb.com>
2025-04-01 11:53:45 +0200

    nvim: enable osc52 for * clipboard

---
+++ config/nvim/lua/config/clipboard.lua
@@ -0,0 +1,17 @@
+-- any copy will go to + clipboard shared with system
+-- vim.opt.clipboard = 'unnamedplus'
+
+-- explanation:
+-- the registers + and *
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = paste,
+    ['*'] = paste,
+  },
+}
+
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -53,16 +53,4 @@ local function paste()
   }
 end
 
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = paste,
-    ['*'] = paste,
-  },
-}
-
 return M
