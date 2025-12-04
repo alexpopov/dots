5e56b86633c93842639312a9858aa6abc03cbc2b
Alex Popov <alexpopov@meta.com>
2025-12-04 05:02:11 -0800

    nvim: new oil plugin for basic file management

--- config/nvim/lua/plugins/monolithic.lua
+++ config/nvim/lua/plugins/monolithic.lua
@@ -331,6 +331,17 @@ local specs = {
       vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {},
+    -- Optional dependencies
+      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+  }
 
 }
 
