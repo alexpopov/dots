01b6bbb2b6dab8f0a7f52aa9be758dc81294016a
Alex Popov <hello@alexpopov.ca>
2026-01-11 15:36:36 +0100

    nvim: shader and hlsl support

---
+++ config/nvim/ftplugin/hlsl.lua
@@ -0,0 +1,5 @@
+local my_opt = require('options')
+
+my_opt.set_tabs(4)
+
+vim.bo.commentstring = '//%s'
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ local ensure_installed
 if (update_treesitter == true)  then
   ensure_installed = {
           "bash", "c", "cpp", "lua", "rust", "cmake", "comment", "go", "java", "javascript", "json",
-          "make", "python", "regex", "vim", "yaml", "kotlin", "markdown", "markdown_inline"
+          "make", "python", "regex", "vim", "yaml", "kotlin", "markdown", "markdown_inline", "hlsl"
         }
 else
   ensure_installed = {}
@@ -13,7 +13,7 @@ end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = true,
+    -- lazy = true,
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = ensure_installed,
