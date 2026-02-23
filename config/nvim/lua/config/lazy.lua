63f3d61071d8a0c8cb74bccd1f4f117691d3731f
Alex Popov (VROS) <alexpopov@meta.com>
2026-02-23 21:47:35 +0100

    nvim: enable vim.loader for Lua byte-code caching

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -1,3 +1,6 @@
+-- Enable Lua byte-code cache for faster subsequent startups
+vim.loader.enable()
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
