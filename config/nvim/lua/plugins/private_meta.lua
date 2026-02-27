55c625615d7ecd388176e0ae02120aa56fa538f2
Alex Popov <alexpopov@meta.com>
2026-02-27 03:10:42 -0800

    nvim: add meta task-oil to private_meta lua plugins

--- config/nvim/lua/plugins/private_meta.lua
+++ config/nvim/lua/plugins/private_meta.lua
@@ -1,6 +1,18 @@
   if os.getenv("ENABLE_PRIVATE_FACEBOOK")
   then
-    return { "meta", dir = "/home/alexpopov/fbsource/fbcode/editor_support/nvim", }
+    return {
+      { "meta", dir = "/home/alexpopov/fbsource/fbcode/editor_support/nvim" },
+      {
+        dir = "~/fbsource/fbcode/scripts/alexpopov/task-oil.nvim",
+        cmd = "TaskOil",
+        keys = {
+          { "<leader>to", "<cmd>TaskOil<cr>", desc = "Task Oil" },
+          { "<leader>tp", "<cmd>TaskOil priority<cr>", desc = "Task Oil (priority)" },
+          { "<leader>ts", "<cmd>TaskOil progress<cr>", desc = "Task Oil (progress)" },
+        },
+        opts = {},
+      },
+    }
   else
     return {}
   end
