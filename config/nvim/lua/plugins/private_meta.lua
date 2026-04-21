537e7b577bf2674cb7d2018bbf589edb8123cc0f
Alex Popov <alexpopov@meta.com>
2026-04-21 02:34:02 -0700

    Update script paths from fbcode/scripts to fbsource/users
    Summary:
    Personal scripts moved from fbcode/scripts/alexpopov/ to
    fbsource/users/al/alexpopov/. Update nvim references:
    - FZF shortcut (\es) points to new location
    - task-oil.nvim plugin loaded from new location
    
    Test Plan:
    Paths verified to exist after fbsource commit lands.
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- config/nvim/lua/plugins/private_meta.lua
+++ config/nvim/lua/plugins/private_meta.lua
@@ -3,7 +3,7 @@
     return {
       { "meta", dir = "/home/alexpopov/fbsource/fbcode/editor_support/nvim" },
       {
-        dir = "~/fbsource/fbcode/scripts/alexpopov/task-oil.nvim",
+        dir = "~/fbsource/fbsource/users/al/alexpopov/task-oil.nvim",
         cmd = "TaskOil",
         keys = {
           { "<leader>to", "<cmd>TaskOil<cr>", desc = "Task Oil" },
